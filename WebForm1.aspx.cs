4cd4cba0879dac89a5efc6f8fc94ca4374c08ba4
alinajawaid <42488001+alinajawaid@users.noreply.github.com>
2018-08-18 12:36:14 +0500

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ajax_Example
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { 
+            if(!IsPostBack)
+            {
+                string query = "selected country_id, country_name from country";
+                BinddropDownList(ddlcountries, query, "country_name", "country_id", "Select country");
+                ddlstate.Enabled = false;
+                ddlCity.Enabled = false;
+                ddlstate.Item.Insert()
+            }
+
+        }
+
+        private void BinddropDownList(DropDownList ddl, string text, string value,string defaulttext)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["Database1"].ConnectionString;
+            SqlCommand cmd = new SqlCommand();
+            using(SqlDataAdapter sda= new SqlDataAdapter())
+            {
+                cmd
+            }
+        }
+    }
+}
